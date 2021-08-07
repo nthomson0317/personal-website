c9c6a7c6a7fe2791ee5de0a93a36a66854eecadd
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-08-07 10:56:04 -0400

    orchid deployed

--- src/components/ProjectsList.jsx
+++ src/components/ProjectsList.jsx
@@ -14,7 +14,7 @@ export default function ProjectsList(props) {
 
 
 
-    let projects = [{name: "Orchid", url: "https://www.youtube.com/embed/lUjXOi96-VU", thumbnail: orchid, description: 'A Spotify-like app designed for classical music'},
+    let projects = [{name: "Orchid", url: "https://www.youtube.com/embed/lUjXOi96-VU", thumbnail: orchid, description: 'A Spotify-like app designed for classical music', link: 'https://orchid-app.herokuapp.com/'},
       {name: "Open Chessame", url:'https://www.youtube.com/embed/dW_hUm2-UtY', thumbnail: 'https://cdn.dribbble.com/users/230124/screenshots/5350574/knight.jpg?compress=1&resize=400x300', description: 'A user-based app for players to improve their chess openings.', link: 'https://open-chessame.herokuapp.com/'},
       {name: "Beanies, Baby!", url: 'https://www.youtube.com/embed/SjcJoyO0lHI', thumbnail: 'https://tycdn.azureedge.net/static/dist/client/img/ty-logo-lg.07ca167.png', description: 'An e-commerce site that allows users to shop for and purchase beanie babies.', link: ''}, 
       {name: "Mega Bytes", url: '', thumbnail: 'https://media.istockphoto.com/vectors/creative-hamburger-logo-design-symbol-vector-illustration-vector-id1156464773?k=6&m=1156464773&s=170667a&w=0&h=mvSWZ1-0BJXoWR5AYvrLMm0-pjvsqEy7UPdo8iOONmI=', description: 'An Instagram-like app for recipes', link: ''}]
