58f3c59545dc99efe3454e23603f30a2f12be0a6
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-08-19 11:08:36 -0400

    fixed timeline

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -17,14 +17,26 @@ class Education extends Component {
                             <div className={classes.row}>
                                 <div className={classes.row_md_12}>
                                     <div className={classes.timeline_centered}>
+                                    <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible ={true}>
+                                            <article>
+                                                <div className={`${classes.timeline_icon} ${classes.timeline_icon_4}`} >
+                                                    <MdWork />     
+                                                </div>
+                                                <div className={classes.label}>
+                                                <h2 >Technical Writer at Datadog <span>August 2021 - present</span></h2>
+                                                <p>Pitch, research, write, and occasionally edit articles for Datadog’s main blog (aka “The Monitor”), which covers Datadog in depth and provides valuable information to the broader tech community around observability, best practices, and industry trends.</p>
+                                                </div>
+                
+                                            </article>
+                                        </ScrollAnimation>
                                         <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible ={true}>
                                             <article className={classes.timeline_entry}>
                                                 <div className={`${classes.timeline_icon} ${classes.timeline_icon_2}`} >
                                                     <FaSchool />     
                                                 </div>
                                                 <div className={classes.label}>
-                                                <h2 >Higher Education  <span>Present</span></h2>
-                                                <p> I am enrolled in the immersive Software Engineering Program at the Flatiron School, studying to become a full-stack developer. </p>
+                                                <h2 >Higher Education  <span>April 2021 - August 2021</span></h2>
+                                                <p> I completed the immersive Software Engineering Program at the Flatiron School, where I learned JavaScript, React, Redux, Ruby, Rails, PostgreSQL, and much more. </p>
                                                 </div>
                                             </article>
                                         </ScrollAnimation>
@@ -37,20 +49,10 @@ class Education extends Component {
                                                 <h2 >Undergrad <span>2007-2011</span></h2>
                                                 <p>I got my B.A. in philosophy from the University of Chicago</p>
                                                 </div>
+                                                  <div className={classes.timeline_entry_inner}><div  className={classes.timeline_icon_3||classes.color_none}></div></div>
                                             </article>
                                         </ScrollAnimation>
-                                        <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible ={true}>
-                                            <article>
-                                                <div className={`${classes.timeline_icon} ${classes.timeline_icon_4}`} >
-                                                    <MdWork />     
-                                                </div>
-                                                <div className={classes.label}>
-                                                <h2 >Technical Writer at Datadog</h2>
-                                                <p>Pitch, research, write, and occasionally edit articles for Datadog’s main blog (aka “The Monitor”), which covers Datadog in depth and provides valuable information to the broader tech community around observability, best practices, and industry trends.</p>
-                                                </div>
-                                                <div className={classes.timeline_entry_inner}><div  className={classes.timeline_icon_3||classes.color_none}></div></div>
-                                            </article>
-                                        </ScrollAnimation>
+
                                     </div>
                                 </div>
                             </div>
